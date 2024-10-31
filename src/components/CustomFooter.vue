30ec05433c26f62c9fa37fb5d4a96bfdbb3cffd6
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-31 00:44:42 -0700

    feat: custom footer update: landing layout

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-primary text-white p-4 text-center">
+    <p>&copy; {{ new Date().getFullYear() }} Sergio Barreras. Todos los derechos reservados.</p>
+  </footer>
+</template>
--- src/layout/LandingLayout.vue
+++ src/layout/LandingLayout.vue
@@ -2,8 +2,11 @@
   <custom-header />
 
   <RouterView />
+
+  <custom-footer />
 </template>
 
 <script lang="ts" setup>
+import CustomFooter from '@/components/CustomFooter.vue';
 import CustomHeader from '@/components/CustomHeader.vue';
 </script>
