b01a1c9dc0eb5387ca1e176412246fb5266a566b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-11-04 16:04:41 -0700

    feat: botones en carrusel

--- src/components/CustomCarousel.vue
+++ src/components/CustomCarousel.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="carousel w-full">
-    <div
-      v-for="(slide, index) in slides"
-      :key="index"
-      :id="`slide${index + 1}`"
-      class="carousel-item relative w-full"
-    >
-      <img :src="slide" class="w-full rounded-xl" />
+    <div class="carousel-item relative w-full">
+      <img :src="slides[index]" class="w-full rounded-xl" />
       <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-        <a
-          :href="`#slide${((index - 1 + slides.length) % slides.length) + 1}`"
-          class="btn btn-circle"
-        >
-          ❮
-        </a>
-        <a :href="`#slide${((index + 1) % slides.length) + 1}`" class="btn btn-circle"> ❯ </a>
+        <button @click="onPreviousSlide" class="btn btn-circle">❮</button>
+        <button @click="onNextSlide" class="btn btn-circle">❯</button>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const index = ref(0);
+
 interface Props {
   slides: string[];
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const onPreviousSlide = () => {
+  if (index.value === 0) index.value = props.slides.length - 1;
+  else index.value--;
+};
+
+const onNextSlide = () => {
+  if (index.value === props.slides.length - 1) index.value = 0;
+  else index.value++;
+};
 </script>
